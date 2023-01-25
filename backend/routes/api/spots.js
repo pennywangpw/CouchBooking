89c6a73f141ccee6fd236966d3c85cc169e489d1
penny_w <penny0love.hotmail.com>
2023-01-25 06:29:05 -0800

    update options in all migration

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { User, Spot, Review } = require('../../db/models');
 const router = express.Router();
-const { Spot } = require('../../db/models');
-const { Review } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
