3325d303dcf5a62b70130a52fe475a4fa9f9ee9d
penny_w <penny0love.hotmail.com>
2023-01-25 06:51:48 -0800

    update options in migration again

--- backend/db/migrations/20230125070508-create-review-image.js
+++ backend/db/migrations/20230125070508-create-review-image.js
@@ -33,7 +33,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable(options);
