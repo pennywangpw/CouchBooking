f00e33ebb34898d822569868726cd0c1897b2578
penny_w <penny0love.hotmail.com>
2023-01-25 14:24:25 -0800

    update model review

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Review.hasMany(
-        models.Review,{foreignKey: 'reviewId',onDelete: 'CASCADE', hooks: true}
+        models.Spot,{foreignKey: 'reviewId',onDelete: 'CASCADE', hooks: true}
       )
 
       //doesn't work for me
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
-const { User, Spot, Review } = require('../../db/models');
+const { User, Spot, Review, SpotImage } = require('../../db/models');
 const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
@@ -9,36 +9,35 @@ const Sequelize = require('sequelize')
 
 
 //Get all Spots
-router.get('/', async (req,res)=>{
-    const spots = await Spot.findAll()
-    let spotsArray= []
-    for(let spot of spots){
-        console.log('spot: ', spot)
-        const spotJSON = spot.toJSON()
-
-        let reviews = await Review.findAll({
-            where:{spotId: spotJSON.id},
-            attributes:[[Sequelize.fn('AVG', Sequelize.col('stars')),'avRating']]
-        })
-        spotJSON.avgRating = reviews[0].toJSON().avgRating
-        spotsArray.push(spotJSON)
-    }
-
-    // const returnSpotId = {
-    //     "ownerId":req.user.id,
-    //     "address": spotId.address,
-    //     "city":spotId.city,
-    //     "state":spotId.state,
-    //     "country":spotId.country,
-    //     "lat":spotId.lat,
-    //     "lng":spotId.lng,
-    //     "name":spotId.name,
-    //     "description":spotId.description,
-    //     "price":spotId.price
-    // }
-
-    return res.json({Sport: spotsArray})
+router.get('/', async(req,res)=>{
+    //get all the columns with associate talbe
+    const allspots = await Spot.findAll({
+        include:[
+            {model: Review},{model: SpotImage}
+        ]
+    })
+    console.log(allspots)
 })
+
+
+// //solution1
+// router.get('/', async (req,res)=>{
+//     const spots = await Spot.findAll()
+//     let spotsArray= []
+//     for(let spot of spots){
+//         console.log('spot: ', spot)
+//         const spotJSON = spot.toJSON()
+
+//         let reviews = await Review.findAll({
+//             where:{spotId: spotJSON.id},
+//             attributes:[[Sequelize.fn('AVG', Sequelize.col('stars')),'avRating']]
+//         })
+//         spotJSON.avgRating = reviews[0].toJSON().avgRating
+//         spotsArray.push(spotJSON)
+//     }
+
+//     return res.json({Sport: spotsArray})
+// })
 
 
 //Get all spots owned by the current user
@@ -53,11 +52,24 @@ router.get('/current', requireAuth, async (req,res)=>{
 })
 
 
-//Get details of a Spot from an id
-router.get('/:spotId', async (req,res)=>{
-    const allspots = await Spot.findAll()
-    console.log(allspots)
-})
+// //Get details of a Spot from an id
+// router.get('/:spotId', async (req,res)=>{
+//     const allspots = await Spot.findAll()
+//     console.log(allspots)
+// })
+
+//create a spot
+// router.post('/', requireAuth, async (req,res)=>{
+//     "address": req.body.address,
+//     "city": "San Francisco",
+//     "state": "California",
+//     "country": "United States of America",
+//     "lat": 37.7645358,
+//     "lng": -122.4730327,
+//     "name": "App Academy",
+//     "description": "Place where web developers are created",
+//     "price": 123
+// })
 
 //delete a spot
 router.delete(':spotId',(req,res)=>{
