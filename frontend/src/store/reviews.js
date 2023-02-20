62ba145f1806a7a2eba6614e814c2df3ce9c689d
penny_w <penny0love.hotmail.com>
2023-02-20 04:01:12 -0800

    work on checklist

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -4,20 +4,20 @@ import { csrfFetch } from './csrf';
 
 
 //constant
-const GET_Reviews ='spots/getReviews'
-const POST_createReview ='spots/createReview'
+const GET_Reviews = 'spots/getReviews'
+const POST_createReview = 'spots/createReview'
 
 //action creator
-export const getReviewsbySpot =(reviews) =>{
+export const getReviewsbySpot = (reviews) => {
     console.log("看看getreveiwsbyspot passed in: ", reviews)
-    return{
+    return {
         type: GET_Reviews,
         reviews
     }
 }
 
-export const createReview =(review) =>{
-    return{
+export const createReview = (review) => {
+    return {
         type: POST_createReview,
         review
     }
@@ -25,25 +25,25 @@ export const createReview =(review) =>{
 
 
 //THUNK - get the review by spot
-export const getReviews = (id) => async (dispatch)=>{
+export const getReviews = (id) => async (dispatch) => {
     console.log("是否有hit getreview ")
     const response = await csrfFetch(`/api/spots/${id}/reviews`)
     const data = await response.json()
-    console.log("THUNK---getReviews: ",data)
+    console.log("THUNK---getReviews: ", data)
     dispatch(getReviewsbySpot(data))
     return data
 }
 
 //THUNK - creat a review for a spot based on the spot's id
-export const createAReview = ({id,newReview}) => async(dispatch) =>{
+export const createAReview = ({ id, newReview }) => async (dispatch) => {
     console.log("有hit createAReview", typeof id, newReview)
-    const response = await csrfFetch(`/api/spots/${id}/reviews`,{
-        method:"POST",
-        headers: {"Content-Type": "application/json"},
+    const response = await csrfFetch(`/api/spots/${id}/reviews`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newReview)
     })
 
-    if(response.ok){
+    if (response.ok) {
         const newReview = await response.json()
         console.log("THUNK----createReview: ", newReview)
         dispatch(createReview(newReview))
@@ -52,11 +52,11 @@ export const createAReview = ({id,newReview}) => async(dispatch) =>{
 }
 
 const initialState = {};
-const reviewsReducer = (state =initialState, action) =>{
+const reviewsReducer = (state = initialState, action) => {
     console.log("Reducer---reviewsReducer with action: ", action)
 
     let newState;
-    switch(action.type){
+    switch (action.type) {
 
         case GET_Reviews:
             // newState = {...state}
@@ -64,16 +64,17 @@ const reviewsReducer = (state =initialState, action) =>{
             // action.reviews.Reviews.forEach(review=> newObj[review.id] = review)
             // console.log("有hit這個條件")
             // return{...state, newState}
-            newState ={}
-            action.reviews.Reviews.forEach(review =>{
-             newState[review.id] = review
+            newState = {}
+            action.reviews.Reviews.forEach(review => {
+                newState[review.id] = review
             })
 
             return newState
 
         case POST_createReview:
-            newState = {}
-
+            newState = { ...state }
+            newState[action.review.id] = action.review
+            return newState
         default:
             return state
     }
