da1e47ccb1a82a92717624393836d11b11e9f2bd
penny_w <penny0love.hotmail.com>
2023-03-10 03:29:00 -0800

    add a div in AllSpots for git push testing

--- frontend/src/components/Spots/AllSpots/AllSpots.js
+++ frontend/src/components/Spots/AllSpots/AllSpots.js
@@ -32,7 +32,7 @@ const AllSpots = () => {
 
   return (
     <div className='allSpotImg'>
-
+      <div>testagain</div>
       {spotsArr.map((spot) => (
         // <div key={id}><NavLink to ={`/spots/${id}`}>{previewImage}</NavLink></div>
         <div className="spotImgOutter" key={spot.id}>
